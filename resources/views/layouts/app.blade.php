8e1d59894207fb4711378bd8e969b77d9413d55d
zaidanlazy <ZaidanMau@gmail.com>
2025-05-10 21:21:37 +0800

    perubahan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,14 +21,40 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMcK7zYtT96ihQFdof4lsKI2pOErtr4GXgo8kbV" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('styles')
 </head>
 <body>
-    <div id="app">
-        @include('layouts.navigation')
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+      <div class="container">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">Perpustakaan</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('user.index') }}">User</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('kategori.index') }}">Kategori</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('buku.index') }}">Buku</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('peminjaman.index') }}">Peminjaman</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
         <main class="py-4">
             @yield('content')
         </main>
